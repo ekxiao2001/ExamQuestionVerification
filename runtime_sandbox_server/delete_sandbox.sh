e781a99cd27c608024bddaa3a0d50387cca0abfa
ekxiao <480583241@qq.com>
2025-11-02 00:41:27 +0800

    fix: 修复容器 ID 获取命令中的正则表达式匹配

--- runtime_sandbox_server/delete_sandbox.sh
+++ runtime_sandbox_server/delete_sandbox.sh
@@ -6,7 +6,7 @@
 # 定义前缀，对应 conf.env 中的 CONTAINER_PREFIX_KEY
 PREFIX="agent-runtime-container-"
 # 获取所有匹配的容器 ID
-CONTAINER_IDS=$(docker ps -a | grep "^$PREFIX" | awk '{print $1}')
+CONTAINER_IDS=$(docker ps -a | grep "$PREFIX" | awk '{print $1}')
 # 停止并删除容器
 if [ -n "$CONTAINER_IDS" ]; then
     echo "停止容器: $CONTAINER_IDS"
